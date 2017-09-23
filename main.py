5d4b936dbdc39977cea7ad5ccd774b498e4cf0be
bigmambo <matt_games@btinternet.com>
2017-09-23 14:03:12 +0100

    Install gaugett and wiringPi and fix gaugett code

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import spotipy.util as util
 from req import Req
 import RPi.GPIO as GPIO
 import gaugette.rotary_encoder
+import gaugette.gpio
 import json
 import time
 
@@ -12,7 +13,8 @@ scope = "streaming playlist-read-private playlist-read-collaborative user-librar
 token = util.prompt_for_user_token(username,scope,client_id='a7864bbc8098453b903abd5008c5a38e',client_secret='4d7d1c6f760c49b4b858572921f9e649',redirect_uri='http://localhost:8888/callback')
 sp = spotipy.Spotify(auth=token)
 
-encoder = gaugette.rotary_encoder.RotaryEncoder.Worker(7, 10)
+gpio = gaugette.gpio.GPIO()
+encoder = gaugette.rotary_encoder.RotaryEncoder.Worker(gpio, 7, 10)
 encoder.start()
 
 chan_list = [8]
