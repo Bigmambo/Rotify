b9e50ae3e326981e71f586af66ad04d743483b4e
Bigmambo <matt_games@btinternet.com>
2017-09-23 22:06:30 +0100

    Clear old library and impliment rotary functions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import spotipy
 import spotipy.util as util
 from req import Req
 import RPi.GPIO as GPIO
-import gaugette.rotary_encoder
+import threading
 import json
 import time
 
@@ -12,13 +12,39 @@ scope = "streaming playlist-read-private playlist-read-collaborative user-librar
 token = util.prompt_for_user_token(username,scope,client_id='a7864bbc8098453b903abd5008c5a38e',client_secret='4d7d1c6f760c49b4b858572921f9e649',redirect_uri='http://localhost:8888/callback')
 sp = spotipy.Spotify(auth=token)
 
-encoder = gaugette.rotary_encoder.RotaryEncoder.Worker(A_PIN, B_PIN)
-encoder.start()
+Button_A = 8
+Enc_A1 = 7
+Enc_A2 = 10
 
-chan_list = [8]
+chan_list = [Button_A, Enc_A1, Enc_A2]
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(chan_list, GPIO.IN)
-GPIO.add_event_detect(8, GPIO.RISING, callback=play_pause, bouncetime=200)
+GPIO.add_event_detect(Button_A, GPIO.RISING, callback=play_pause, bouncetime=200)
+GPIO.add_event_detect(Enc_A1, GPIO.RISING, callback=rotary_interrupt)
+GPIO.add_event_detect(Enc_A2, GPIO.RISING, callback=rotary_interrupt)
+
+delta = 0
+Current_A1 = 1
+Current_A2 = 1
+LockRotary = threading.Lock()
+
+def rotary_interrupt(A_or_B):
+	global delta, Current_A1, Current_A2, LockRotary
+    Switch_A1 = GPIO.input(Enc_A1)
+    Switch_A2 = GPIO.input(Enc_A2)
+    if Current_A1 == Switch_A1 and Current_A2 == Switch_A2:
+        return
+    Current_A1 = Switch_A1
+    Current_A2 = Switch_A2
+    if (Switch_A1 and Switch_A2):
+		LockRotary.acquire()
+		if A_or_B == Enc_A2:
+			delta += 1
+		else:
+			delta -= 1
+		LockRotary.release()
+	return
+
 
 def volume():
     v = get('/v1/me/player')
@@ -36,7 +62,6 @@ def play_pause(channel):
 
 def volume_knob():
     v = volume()
-    delta = encoder.get_delta()
     if delta!=0:
         vol = (5 * (v/5)) + (delta * 5)
         if vol < 0:
@@ -44,6 +69,9 @@ def volume_knob():
         elif vol > 100:
             vol = 100
     put('/v1/me/player/volume', vol)
+    delta = 0
 
-while True:
-    volume = volume_knob()
+def main():
+    while True:
+        time.sleep(0.1)
+        volume = volume_knob()
