d353b3a7560a68fbbcc6e7cb709b40a31647d016
bigmambo <matt_games@btinternet.com>
2017-09-23 22:28:40 +0100

    Fix indents

--- main.py
+++ main.py
@@ -29,9 +29,11 @@ Current_A2 = 1
 LockRotary = threading.Lock()
 
 def rotary_interrupt(A_or_B):
-	global delta, Current_A1, Current_A2, LockRotary
+    global delta, Current_A1, Current_A2, LockRotary
+
     Switch_A1 = GPIO.input(Enc_A1)
     Switch_A2 = GPIO.input(Enc_A2)
+
     if Current_A1 == Switch_A1 and Current_A2 == Switch_A2:
         return
     Current_A1 = Switch_A1
@@ -43,7 +45,7 @@ def rotary_interrupt(A_or_B):
 		else:
 			delta -= 1
 		LockRotary.release()
-	return
+    return
 
 
 def volume():
