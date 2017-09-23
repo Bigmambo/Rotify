42cbcf06b8550f33842fe69f519864e416630e69
Bigmambo <matt_games@btinternet.com>
2017-09-23 16:17:03 +0100

    Revert to a5e0af2

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import spotipy.util as util
 from req import Req
 import RPi.GPIO as GPIO
 import gaugette.rotary_encoder
-import gaugette.gpio
 import json
 import time
 
@@ -13,8 +12,7 @@ scope = "streaming playlist-read-private playlist-read-collaborative user-librar
 token = util.prompt_for_user_token(username,scope,client_id='a7864bbc8098453b903abd5008c5a38e',client_secret='4d7d1c6f760c49b4b858572921f9e649',redirect_uri='http://localhost:8888/callback')
 sp = spotipy.Spotify(auth=token)
 
-gpio = gaugette.gpio.GPIO()
-encoder = gaugette.rotary_encoder.RotaryEncoder.Worker(gpio, 7, 10)
+encoder = gaugette.rotary_encoder.RotaryEncoder.Worker(A_PIN, B_PIN)
 encoder.start()
 
 chan_list = [8]
