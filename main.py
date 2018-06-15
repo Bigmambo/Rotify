317c0d9f7c4d78a6aa06a286cd769df8521bbf57
Bigmambo <matt_games@btinternet.com>
2018-06-15 13:37:00 +0100

    Minor changes

--- main.py
+++ main.py
@@ -113,7 +113,7 @@ def volume():
 
 def play_pause(channel):
     r = req.get('/v1/me/player')
-    play_pause_json = json.dumps(result , indent=4)
+    play_pause_json = json.loads(r)
     if play_pause_json["is_playing"] == "true":
         req.put('/v1/me/player/play')
     else:
